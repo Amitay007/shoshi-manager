6434cc96b7b82abc79de6b3628e7a12cb06c5ed1
יויה מציאות חינוכית חדשה <swift.flyhigh@gmail.com>
2026-01-06 20:26:09 +0000

    File changes

--- src/pages/MasterSchedule.jsx
+++ src/pages/MasterSchedule.jsx
@@ -373,9 +373,9 @@ export default function MasterSchedule() {
                     <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
                   </Button>
                 </PopoverTrigger>
-                <PopoverContent className="w-[var(--radix-popover-trigger-width)] p-0 z-[100]" align="start">
-                  <Command>
-                    <CommandInput placeholder="הקלד לחיפוש..." className="h-9" />
+                <PopoverContent className="w-[500px] p-0" align="start">
+                  <Command className="rounded-lg border shadow-md">
+                    <CommandInput placeholder="הקלד לחיפוש..." />
                     <CommandEmpty>לא נמצא בית ספר.</CommandEmpty>
                     <CommandGroup className="max-h-[300px] overflow-y-auto">
                       {schools.map((school) => (
