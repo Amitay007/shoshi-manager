1101b98b33f75ea9b16cacf228d5bc0965a61193
יויה מציאות חינוכית חדשה <swift.flyhigh@gmail.com>
2026-01-08 04:59:13 +0000

    File changes

--- src/components/ui/toast.jsx
+++ src/components/ui/toast.jsx
@@ -15,7 +15,7 @@ const ToastViewport = React.forwardRef(({ className, ...props }, ref) => (
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed z-[100] flex max-h-screen w-full flex-col-reverse p-4 bottom-0 right-0 top-auto flex-col md:max-w-[420px]",
+      "fixed z-[100] flex max-h-screen w-full flex-col-reverse p-4 bottom-0 right-0 top-auto flex-col md:max-w-[420px] pointer-events-none",
       className
     )}
     {...props}
@@ -66,7 +66,7 @@ const ToastClose = React.forwardRef(({ className, ...props }, ref) => (
   <ToastPrimitives.Close
     ref={ref}
     className={cn(
-      "absolute right-2 top-2 rounded-md p-1 text-slate-950/50 opacity-0 transition-opacity hover:text-slate-950 focus:opacity-100 focus:outline-none focus:ring-2 group-hover:opacity-100 group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600",
+      "absolute left-2 top-2 rounded-md p-1 text-slate-950/50 opacity-100 transition-opacity hover:text-slate-950 focus:opacity-100 focus:outline-none focus:ring-2 group-hover:opacity-100 group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600",
       className
     )}
     toast-close=""
