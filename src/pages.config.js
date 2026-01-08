ddcdb89711af37f207811bce12de3022110085b4
יויה מציאות חינוכית חדשה <swift.flyhigh@gmail.com>
2026-01-08 02:42:29 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -26,6 +26,7 @@ import Humanmanagement from './pages/Humanmanagement';
 import MasterSchedule from './pages/MasterSchedule';
 import MySchedule from './pages/MySchedule';
 import ProgramView from './pages/ProgramView';
+import Programs from './pages/Programs';
 import ResearchPage from './pages/ResearchPage';
 import SchoolDetails from './pages/SchoolDetails';
 import Schools from './pages/Schools';
@@ -38,7 +39,6 @@ import UpdateAppStatus from './pages/UpdateAppStatus';
 import UpdateAppsFromPDF from './pages/UpdateAppsFromPDF';
 import UploadLogo from './pages/UploadLogo';
 import Version2 from './pages/Version2';
-import Programs from './pages/Programs';
 import __Layout from './Layout.jsx';
 
 
@@ -71,6 +71,7 @@ export const PAGES = {
     "MasterSchedule": MasterSchedule,
     "MySchedule": MySchedule,
     "ProgramView": ProgramView,
+    "Programs": Programs,
     "ResearchPage": ResearchPage,
     "SchoolDetails": SchoolDetails,
     "Schools": Schools,
@@ -83,7 +84,6 @@ export const PAGES = {
     "UpdateAppsFromPDF": UpdateAppsFromPDF,
     "UploadLogo": UploadLogo,
     "Version2": Version2,
-    "Programs": Programs,
 }
 
 export const pagesConfig = {
