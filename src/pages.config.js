ccba2bc37a2d381e4af27cdfd0e97667d1ac9a43
יויה מציאות חינוכית חדשה <swift.flyhigh@gmail.com>
2026-01-12 08:36:17 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,6 +1,4 @@
 import AccountsAndUsers from './pages/AccountsAndUsers';
-import AddAppPage from './pages/AddAppPage';
-import AddAppToDevice from './pages/AddAppToDevice';
 import AddAppsFromList from './pages/AddAppsFromList';
 import AddNewHeadset from './pages/AddNewHeadset';
 import AppDetailsPage from './pages/AppDetailsPage';
@@ -9,7 +7,6 @@ import AssignmentDetails from './pages/AssignmentDetails';
 import BinocularCalculator from './pages/BinocularCalculator';
 import BulkDataLoader from './pages/BulkDataLoader';
 import CRMHub from './pages/CRMHub';
-import CashFlow from './pages/CashFlow';
 import ContactDetails from './pages/ContactDetails';
 import CreateProgram from './pages/CreateProgram';
 import Dashboard from './pages/Dashboard';
@@ -19,16 +16,12 @@ import DataRepositoryList from './pages/DataRepositoryList';
 import DataUpdater from './pages/DataUpdater';
 import DeviceAssignments from './pages/DeviceAssignments';
 import DeviceInfo from './pages/DeviceInfo';
-import DeviceMenu from './pages/DeviceMenu';
-import EditHeadset from './pages/EditHeadset';
 import GeneralApps from './pages/GeneralApps';
 import GeneralInfo from './pages/GeneralInfo';
 import Home from './pages/Home';
 import HoursReport from './pages/HoursReport';
 import Management from './pages/Management';
 import ManagerScheduler from './pages/ManagerScheduler';
-import MasterSchedule from './pages/MasterSchedule';
-import MySchedule from './pages/MySchedule';
 import ProgramView from './pages/ProgramView';
 import Programs from './pages/Programs';
 import RelationalCenter from './pages/RelationalCenter';
@@ -45,13 +38,12 @@ import UpdateAppsFromPDF from './pages/UpdateAppsFromPDF';
 import UploadLogo from './pages/UploadLogo';
 import Version2 from './pages/Version2';
 import TeacherProfile from './pages/TeacherProfile';
+import CashFlow from './pages/CashFlow';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
     "AccountsAndUsers": AccountsAndUsers,
-    "AddAppPage": AddAppPage,
-    "AddAppToDevice": AddAppToDevice,
     "AddAppsFromList": AddAppsFromList,
     "AddNewHeadset": AddNewHeadset,
     "AppDetailsPage": AppDetailsPage,
@@ -60,7 +52,6 @@ export const PAGES = {
     "BinocularCalculator": BinocularCalculator,
     "BulkDataLoader": BulkDataLoader,
     "CRMHub": CRMHub,
-    "CashFlow": CashFlow,
     "ContactDetails": ContactDetails,
     "CreateProgram": CreateProgram,
     "Dashboard": Dashboard,
@@ -70,16 +61,12 @@ export const PAGES = {
     "DataUpdater": DataUpdater,
     "DeviceAssignments": DeviceAssignments,
     "DeviceInfo": DeviceInfo,
-    "DeviceMenu": DeviceMenu,
-    "EditHeadset": EditHeadset,
     "GeneralApps": GeneralApps,
     "GeneralInfo": GeneralInfo,
     "Home": Home,
     "HoursReport": HoursReport,
     "Management": Management,
     "ManagerScheduler": ManagerScheduler,
-    "MasterSchedule": MasterSchedule,
-    "MySchedule": MySchedule,
     "ProgramView": ProgramView,
     "Programs": Programs,
     "RelationalCenter": RelationalCenter,
@@ -96,6 +83,7 @@ export const PAGES = {
     "UploadLogo": UploadLogo,
     "Version2": Version2,
     "TeacherProfile": TeacherProfile,
+    "CashFlow": CashFlow,
 }
 
 export const pagesConfig = {
